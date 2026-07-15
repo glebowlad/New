ae0e502b6df671d7cb861c66d5e717b0931247cd
Delteltron <vladimirlitvinov487@gmail.com>
2026-07-15 12:20:35 +0300

    Script

--- Assets/New.cs
+++ Assets/New.cs
@@ -7,7 +7,7 @@ public class New : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0, 1, 0);
+        transform.Rotate(0, 2, 0);
     }
 
     // Update is called once per frame
